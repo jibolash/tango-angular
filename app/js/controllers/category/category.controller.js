83673eb54f511f9df6aca5c6bb6e63bcef84e7e7
Stephen <stephen.sunday@andela.co>
2015-06-08 21:43:10 +0100

    Remove debbuging data

--- app/js/controllers/category/category.controller.js
+++ app/js/controllers/category/category.controller.js
@@ -3,14 +3,8 @@
 var app = angular.module("Tango");
 
 app.controller("categoryController", ['categoryService','$scope',function(categoryService,$scope){
-	var v;
 	categoryService.getAll()
 		.success(function(data){
 			$scope.categories = data;
-			v = data;
 		})
-
-		setTimeout(function(){
-			console.log(v[0].iconLink);
-		},2000)
 }])
