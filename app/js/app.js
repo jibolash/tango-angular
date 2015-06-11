fb8961605ddfb6b8999df9b0641d2830708df57f
Jibola Sogbein <ajibola.sogbein@andela.co>
2015-06-12 00:53:20 +0100

    change gigs page

--- app/js/app.js
+++ app/js/app.js
@@ -1,36 +1,34 @@
 'use strict';
 
-angular.module("Tango",['ngMaterial','ngRoute','ui.router'])
-	.config(function($httpProvider, $mdThemingProvider){
-    $httpProvider.interceptors.push('AuthInterceptor');
+angular.module("Tango", ['ngMaterial', 'ngRoute', 'ui.router'])
+    .config(function($httpProvider, $mdThemingProvider) {
+        $httpProvider.interceptors.push('AuthInterceptor');
 
-     $mdThemingProvider.theme('default')
-    .primaryPalette('green')
-    .accentPalette('blue')
-    .backgroundPalette('brown');
-  });
-
+        $mdThemingProvider.theme('default')
+            .primaryPalette('green')
+            .accentPalette('blue')
+            .backgroundPalette('brown');
+    });
 angular.module('Tango').run(['$rootScope', '$location', 'Auth', function($rootScope, $location, Auth) {
-
-  $rootScope.loggedIn = Auth.isLoggedIn();
-  $rootScope.$on('$routeChangeStart', function(event, next, current){
     $rootScope.loggedIn = Auth.isLoggedIn();
-    Auth.getUser().success(function(data){
-          $rootScope.user = data;
+    $rootScope.$on('$routeChangeStart', function(event, next, current) {
+        $rootScope.loggedIn = Auth.isLoggedIn();
+        Auth.getUser().success(function(data) {
+            $rootScope.user = data;
         });
     });
 }]);
 
 angular.module("Tango")
-  .config(function($stateProvider,$urlRouterProvider){
-    $urlRouterProvider.otherwise("/")
-    $stateProvider
-      .state('home',{
-        url:"/",
-        templateUrl: "app/views/landing.html"
-      })
-      .state('add',{
-        url:"/gigs/new",
-        templateUrl: "app/views/addGig.html"
-      })
-  });
+    .config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise("/")
+        $stateProvider
+            .state('home', {
+                url: "/",
+                templateUrl: "app/views/landing.html"
+            })
+            .state('add', {
+                url: "/gigs/new",
+                templateUrl: "app/views/addGig.html"
+            })
+    });
