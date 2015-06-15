59148fe8dc795e3d145f8d0987895c6577906ebc
Stephen <stephen.sunday@andela.co>
2015-06-15 10:29:33 +0100

    Trash changes

--- app/js/services/category.service.js
+++ app/js/services/category.service.js
@@ -5,7 +5,7 @@ var app = angular.module("Tango");
 app.factory("categoryService", ['baseUrl','$http', function(baseUrl,$http){
 	return {
 		getAll: function(){
-			return $http.get(baseUrl+"/categories");
+			return $http.get(baseUrl + "/categories");
 		}
 	}
 }])
