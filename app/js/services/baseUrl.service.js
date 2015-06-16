3d05bc98adedd2e54f500cf573ece601303cd208
Jibola Sogbein <ajibola.sogbein@andela.co>
2015-06-16 11:06:57 +0100

    added order now button to gig detail page

--- app/js/services/baseUrl.service.js
+++ app/js/services/baseUrl.service.js
@@ -5,4 +5,4 @@ var app = angular.module("Tango");
 var localhost = "http://localhost:8000/api";
 var heroku = "https://tangong-api.herokuapp.com/api";
 
-app.value("baseUrl", heroku);
+app.value("baseUrl", localhost);
