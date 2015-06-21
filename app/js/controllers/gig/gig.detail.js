8932ace4f3901e73c9aa1133a15df1266dc18506
Jibola Sogbein <ajibola.sogbein@andela.co>
2015-06-21 14:42:48 +0100

    added twitter button to gig detail page

--- app/js/controllers/gig/gig.detail.js
+++ app/js/controllers/gig/gig.detail.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('Tango')
-  .controller('gigDetailCtrl', ['$scope', 'gigService', '$stateParams', function($scope, gigService, $stateParams){
+  .controller('gigDetailCtrl', ['$scope', 'gigService', '$stateParams', function($scope, gigService, $stateParams) {
 
     gigService.oneGig($stateParams.gigid)
-      .success(function(data){
+      .success(function(data) {
         $scope.gigInfo = data;
+        document.getElementById('twitterLink').setAttribute("data-text", $scope.gigInfo.title);
       });
-}]);
+  }]);
