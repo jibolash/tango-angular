03d901bfc35eca182f7183dc1ca4929db02806b1
Jibola Sogbein <ajibola.sogbein@andela.co>
2015-06-23 10:14:16 +0100

    remove map from gigs that have no address

--- app/js/controllers/gig/gig.detail.js
+++ app/js/controllers/gig/gig.detail.js
@@ -6,14 +6,25 @@ angular.module('Tango')
     $scope.gigid = $stateParams.gigid;
     $scope.url = "http://localhost:3000/#!/gig/" + $stateParams.gigid;
 
+    // gigService.oneGig($stateParams.gigid)
+    //   .success(function(data) {
+    //     $scope.Address = data.address;
+    //     $scope.gigInfo = data;
+    //     var twittermessage = data.title;
+    //     document.getElementById('twitterLink').setAttribute("data-text", twittermessage);
+    //   });
+
     gigService.oneGig($stateParams.gigid)
       .success(function(data) {
-        $scope.Address = data.address;
+        if (data.address) {
+          $scope.showMap = true;
+          $scope.Address = data.address;
+        }
+        else{
+          $scope.showMap = false;
+        }
         $scope.gigInfo = data;
         var twittermessage = data.title;
         document.getElementById('twitterLink').setAttribute("data-text", twittermessage);
       });
-
-
-
   }]);
