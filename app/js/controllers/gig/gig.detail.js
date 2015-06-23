655a235e38e957cb73296d0d2e44ee82d01e08f4
Jibola Sogbein <ajibola.sogbein@andela.co>
2015-06-23 10:46:52 +0100

    changed color of loading bar to green

--- app/js/controllers/gig/gig.detail.js
+++ app/js/controllers/gig/gig.detail.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('Tango')
-  .controller('gigDetailCtrl', ['$scope', 'gigService', '$stateParams','$rootScope', function($scope, gigService, $stateParams,$rootScope) {
+  .controller('gigDetailCtrl', ['$scope', 'gigService', '$stateParams', function($scope, gigService, $stateParams) {
 
     $scope.gigid = $stateParams.gigid;
     $scope.url = "http://localhost:3000/#!/gig/" + $stateParams.gigid;
@@ -16,12 +16,6 @@ angular.module('Tango')
           $scope.showMap = false;
         }
         $scope.gigInfo = data;
-        $scope.t_details
-        // $scope.t_details = {
-        //   'provider' : data.addedBy.username,
-        //   'service' : data.title,
-        //   'user' : $rootScope.user.username
-        // }
         var twittermessage = data.title;
         document.getElementById('twitterLink').setAttribute("data-text", twittermessage);
       });
